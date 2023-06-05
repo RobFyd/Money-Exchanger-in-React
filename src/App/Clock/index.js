2bb642787d62f266a50691a949a11939f5cab777
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-06-06 00:49:20 +0100

    useCurrnetDate hook added

--- src/App/Clock/index.js
+++ src/App/Clock/index.js
@@ -1,8 +1,10 @@
-import { useState, useEffect } from "react";
+import { useCurrentDate } from "./useCurrentDate";
 import { AppClock } from "./styled.js";
 
-const formatDate = (date) =>
-  date.toLocaleString("en-EN", {
+const Clock = () => {
+  const { date } = useCurrentDate();
+
+  const formatDate = date.toLocaleString("en-EN", {
     month: "long",
     weekday: "long",
     day: "numeric",
@@ -11,17 +13,7 @@ const formatDate = (date) =>
     second: "numeric",
   });
 
-export const Clock = () => {
-  const [date, setDate] = useState(new Date());
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setDate(new Date());
-    }, 1000);
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, [date]);
-
-  return <AppClock>Today is {formatDate(date)}</AppClock>;
+  return <AppClock>Today is {formatDate}</AppClock>;
 };
+
+export { Clock };
