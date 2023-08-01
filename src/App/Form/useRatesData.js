08b51ecdf7940d950eed52e6dfb31f84f1a9c5eb
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-08-01 22:44:00 +0100

    comment deleted

--- src/App/Form/useRatesData.js
+++ src/App/Form/useRatesData.js
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-// var requestURL = "https://api.exchangerate.host/latest?base=GBP";
-
 export const useRatesData = () => {
   const [ratesData, setRatesData] = useState({
     state: "running",
